19a6675566bb389f1396730c03b7ea84fecd88c5
Nancy-Muthinzi <nanciekathini@gmail.com>
2018-08-11 22:31:14 +0300

    add background color, font-family and font-size styles

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import {Quote} from '../quote';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote-form',
@@ -7,7 +7,12 @@ import {Quote} from '../quote';
   styleUrls: ['./quote-form.component.css']
 })
 export class QuoteFormComponent implements OnInit {
-  newQuote=new Quote(0,"","",new Date());
+  newQuote = new Quote(0, "", "", new Date());
+  @Output() addQuote = new EventEmitter<Quote>();
+
+  submitQuote() {
+    this.addQuote.emit(this.newQuote);
+  }
 
   constructor() { }
 
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,7 +8,7 @@ import { Quote } from '../quote'
 })
 
 export class QuoteComponent implements OnInit {
-  
+
   quotes = [
     new Quote(1, 'You know you’re in love when you can’t fall asleep because reality is finally better than your dreams.', 'Dr. Seuss', new Date(1944, 3, 5)),
     new Quote(2, 'I’m selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can’t handle me at my worst, then you sure as hell don’t deserve me at my best.', 'Marilyn Monroe', new Date(1934, 12, 15)),
@@ -16,6 +16,7 @@ export class QuoteComponent implements OnInit {
     new Quote(4, 'Get busy living or get busy dying.', 'Stephen King', new Date(1994, 8, 17)),
     new Quote(5, 'Twenty years from now you will be more disappointed by the things that you didn’t do than by the ones you did do.', 'Mark Twain', new Date(1902, 2, 28)),
     new Quote(6, 'When I dare to be powerful – to use my strength in the service of my vision, then it becomes less and less important whether I am afraid.', 'Audre Lorde', new Date(2004, 12, 12)),
+    new Quote(7, 'The noblest art is that of making others happy.', 'P. T. Barnum', new Date(1924,4,4)),
 
   ]
 
@@ -32,7 +33,13 @@ export class QuoteComponent implements OnInit {
       }
     }
   }
+  addNewQuote(quote){
+            let quoteLength = this.quotes.length;
+            quote.id=quoteLength+1;
+            quote.completeDate = new Date(quote.completeDate)
+            this.quotes.push(quote)
 
+        }
     constructor() { }
     ngOnInit() {
     }
